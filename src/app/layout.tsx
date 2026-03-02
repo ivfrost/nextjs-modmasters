60158a9b614bbfa68d40eeb72d1707f72851c467
ivfrost <ivfrost@github.com>
2026-03-02 13:37:23 +0100

    adjust: update project to fit modding website idea checkpoint: checkpoint before stack auth init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { NavBar } from "@/components/nav/nav-bar";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { NavBar } from '@/components/nav/nav-bar';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Wikimasters",
-  description: "Learn how to build and scale Next.js apps with Brian Holt",
+	title: 'Modmasters',
+	description:
+		'Create and share mods for your favorite games with the community',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NavBar />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<NavBar />
+				{children}
+			</body>
+		</html>
+	);
 }
