9c19eac64934af3ffc0ae3f5802ca4610c329496
ivfrost <ivfrost@github.com>
2026-03-11 14:33:51 +0100

    chore: fix linting issues

--- src/hooks/useTruncatedElement.ts
+++ src/hooks/useTruncatedElement.ts
@@ -2,29 +2,29 @@
 import { useLayoutEffect, useState } from 'react';
 
 export default function useTruncatedElement(
-	ref: React.RefObject<HTMLElement> | React.RefObject<null>,
+  ref: React.RefObject<HTMLElement> | React.RefObject<null>,
 ) {
-	if (!ref) {
-		throw new Error('useTruncatedElement requires a ref');
-	}
+  if (!ref) {
+    throw new Error('useTruncatedElement requires a ref');
+  }
 
-	const [isTruncated, setIsTruncated] = useState(false);
-	const [isReadingMore, setIsReadingMore] = useState(false);
+  const [isTruncated, setIsTruncated] = useState(false);
+  const [isReadingMore, setIsReadingMore] = useState(false);
 
-	useLayoutEffect(() => {
-		if (!ref.current) return;
-		// Initial check
-		const { offsetHeight, scrollHeight } = ref.current;
-		if (offsetHeight && scrollHeight && offsetHeight < scrollHeight) {
-			setIsTruncated(true);
-		} else {
-			setIsTruncated(false);
-		}
-	}, [ref]);
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+    // Initial check
+    const { offsetHeight, scrollHeight } = ref.current;
+    if (offsetHeight && scrollHeight && offsetHeight < scrollHeight) {
+      setIsTruncated(true);
+    } else {
+      setIsTruncated(false);
+    }
+  }, [ref]);
 
-	return {
-		isTruncated,
-		isReadingMore,
-		setIsReadingMore,
-	};
+  return {
+    isTruncated,
+    isReadingMore,
+    setIsReadingMore,
+  };
 }
